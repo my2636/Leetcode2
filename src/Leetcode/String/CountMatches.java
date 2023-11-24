0f899393a11fe0b9276bb8d7355c4137dfc0ea46
marina <greenbox22@yandex.ru>
2023-11-24 18:32:52 +0400

    2

---
+++ src/Leetcode/String/CountMatches.java
@@ -0,0 +1,13 @@
+package Leetcode.String;
+
+import java.util.List;
+
+public class CountMatches {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        return 0;
+    }
+}
--- src/Leetcode/String/RestoreString.java
+++ src/Leetcode/String/RestoreString.java
@@ -1,19 +1,22 @@
 package Leetcode.String;
 
+import java.util.Arrays;
+
 public class RestoreString {
     public static void main(String[] args) {
         int[] arr = {4,5,6,7,0,2,1,3};
         System.out.println(restoreString("codeleet", arr));
     }
     public static String restoreString(String s, int[] indices) {
-        for (int i = 1; i < indices.length; i++) {
-            int key = indices[i];
-            int j = i - 1;
-            while (j >= 0 && indices[j] > key) {
-                indices[j+1] = indices[i];
-            }
-            indices[j+1] =key;
+        char[] arr1 = new char[s.length()];
+        String s1 = "";
+        for (int i = 0; i < arr1.length; i++) {
+            arr1[indices[i]] = s.charAt(i);
         }
-        return indices.toString();
+        for (int j = 0; j < arr1.length; j++) {
+            s1 += arr1[j];
+        }
+        System.out.println(new String(arr1));
+        return s1;
     }
 }
